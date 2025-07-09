475f43322ee90e34716d351d37f808ab408ab92d
Renan N Borges <renan.n.borges@hotmail.com>
2025-07-09 10:39:37 -0300

    feat: impls pagination in MessageRepository queries return

--- backend/src/main/java/repository/MessageRepository.java
+++ backend/src/main/java/repository/MessageRepository.java
@@ -1,13 +1,13 @@
 package repository;
 
 import model.Message;
-import model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+    Page<Message> findByUser_Id(UUID userId, Pageable pageable);
 }
