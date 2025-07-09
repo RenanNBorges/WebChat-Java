67f369e059f5997088e09b488a90e5f133992aa4
Renan N Borges <renan.n.borges@hotmail.com>
2025-07-09 10:36:09 -0300

    feat: impls custom query for find user chats in chat repository

--- backend/src/main/java/repository/ChatRepository.java
+++ backend/src/main/java/repository/ChatRepository.java
@@ -1,11 +1,17 @@
 package repository;
 
 import model.Chat;
+import model.ChatMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
+    @Query("SELECT c FROM Chat c JOIN c.members m WHERE m.user.id = :userID")
+    List<ChatMember> findByUser(@Param("userID") UUID userID);
 }
